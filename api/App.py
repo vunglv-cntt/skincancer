7f15b3f72712268447ef8001c4a2436d4ce7aacb
vung-cntt <vunglv@vgsholding.com>
2023-12-26 11:44:54 +0700

    - commit cốt

--- api/App.py
+++ api/App.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required, unset_jwt_cookies
+from flask_jwt_extended import JWTManager, create_access_token, create_refresh_token,get_jwt_identity, jwt_required, unset_jwt_cookies
 from pymongo import MongoClient
 import secrets
 from PIL import Image
@@ -8,34 +8,50 @@ import tensorflow as tf
 import io
 from flask_cors import CORS
 from datetime import timedelta
-secret_key = secrets.token_hex(32)
+from dotenv import load_dotenv
+import os
+from utils import load_model
+
+
 app = Flask(__name__)
 CORS(app)
-app.config['SECRET_KEY'] = secret_key
-bcrypt = Bcrypt(app)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'fallback-secret-key')
 app.config['JWT_TOKEN_LOCATION'] = ['headers']
 app.config['JWT_ALGORITHM'] = 'HS256'
 app.config['JWT_IDENTITY_CLAIM'] = 'sub'
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=30)
+
+bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
-# Kết nối đến MongoDB
-client = MongoClient("mongodb+srv://levanvung113:vungle2001@hive-cluser.zw2amvy.mongodb.net/?retryWrites=true&w=majority")
+
+client = MongoClient(os.getenv('MONGO_URI'))
 db = client["user_name"]
 users_collection = db["hive"]
 predictions_collection = db["predictions"]
+
 @app.route('/api/login', methods=['POST'])
 def api_login():
     data = request.json
     username = data.get('username')
     password = data.get('password')
-
     user = users_collection.find_one({"username": username})
 
     if user and bcrypt.check_password_hash(user['password'], password):
         access_token = create_access_token(identity=username)
-        return jsonify({"idUser": user['idUser'],"status": " success" ,"user_name" :user['username'], "access_token": access_token, }),200
+        refresh_token = create_refresh_token(identity=username)
+        return jsonify({"idUser": user['idUser'],"status": " success" ,"user_name" :user['username'], "access_token": access_token,"refresh_token": refresh_token }),200
     else:
         return jsonify({"error": "Invalid username or password"}), 401
+
+
+@app.route('/api/token/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    current_user = get_jwt_identity()
+    new_token = create_access_token(identity=current_user)
+    return jsonify({'access_token': new_token})
 
 @app.route('/api/signup', methods=['POST'])
 def api_signup():
@@ -44,35 +60,27 @@ def api_signup():
     email = data.get('email')
     password = data.get('password')
 
-    # Kiểm tra xem username đã tồn tại hay chưa
     existing_user = users_collection.find_one({"username": username})
     if existing_user:
         return jsonify({"error": "Username already exists"}), 400
 
-    # Kiểm tra xem email đã tồn tại hay chưa
     existing_email = users_collection.find_one({"email": email})
     if existing_email:
         return jsonify({"error": "Email already exists"}), 400
 
-    # Nếu username và email không tồn tại, tiếp tục đăng ký
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-
     last_user = users_collection.find_one(sort=[("idUser", -1)])
-
     if last_user:
         last_id_user = last_user.get("idUser", 0)
     else:
         last_id_user = 0
-
     new_id_user = last_id_user + 1
-
     user_data = {
         "idUser": new_id_user,
         "username": username,
         "email": email,
         "password": hashed_password
     }
-
     users_collection.insert_one(user_data)
     return jsonify({"message": "Successfully registered!"})
 
@@ -81,16 +89,11 @@ def api_signup():
 def get_user_info():
     current_user = get_jwt_identity()
     user = users_collection.find_one({"username": current_user}, {"_id": 0, "password": 0})
-
     if user:
-        # Trả về thông tin người dùng bao gồm user_name
         return jsonify({"user_name": user.get("username", ""), "idUser": user.get("idUser", "")}), 200
     else:
         return jsonify({"error": "User not found"}), 404
     
-def load_model():
-    model = tf.keras.models.load_model("../model/model.h5")
-    return model
 labels = [
     "actinic keratosis",
     "basal cell carcinoma",
@@ -124,6 +127,7 @@ def predict():
     prediction = prediction[0]
 
     return jsonify({'disease': labels[prediction].title(), 'confidence': f'{score:.2f}%'})
+
 @app.route('/api/store', methods=['POST'])
 def store():
     data = request.json
@@ -149,22 +153,110 @@ def get_predictions_by_user_id():
     if user:
         user_id = user.get("idUser")
 
-        # Thay đổi từ find_one thành find để lấy tất cả các bản ghi
-        predictions = predictions_collection.find({"userId": str(user_id)}, {"_id": 0})
+        # Thêm tham số phân trang với giá trị mặc định
+        page = int(request.args.get('page', 1))
+        page_size = int(request.args.get('page_size', 10))
+        skip = (page - 1) * page_size
 
-        # Chuyển đổi con trỏ dự đoán thành danh sách để sử dụng jsonify
+        # Lấy tổng số bản ghi để tính tổng số trang
+        total_records = predictions_collection.count_documents({"userId": str(user_id)})
+        total_pages = (total_records + page_size - 1) // page_size
+
+        # Truy vấn dữ liệu được phân trang từ MongoDB
+        predictions = predictions_collection.find({"userId": str(user_id)}, {"_id": 0}).skip(skip).limit(page_size)
         predictions_list = list(predictions)
 
-        # Trả về danh sách dự đoán cho người dùng
-        return jsonify(predictions_list), 200
+        # Trả về dữ liệu cùng thông tin phân trang
+        return jsonify({
+            "data": predictions_list,
+            "total_records": total_records,
+            "total_pages": total_pages,
+            "current_page": page,
+            "page_size": page_size
+        }), 200
     else:
         return jsonify({"error": "User not found"}), 404
-# @app.route('/api/logout', methods=['POST'])
-# @jwt_required()
-# def api_logout():
-#     resp = jsonify({"message": "Logout successful"})
-#     unset_jwt_cookies(resp)
-#     return resp, 200
+    
+@app.route('/api/delete_prediction/<int:prediction_id>', methods=['DELETE'])
+@jwt_required()
+def delete_prediction(prediction_id):
+    current_user = get_jwt_identity()
+    user = users_collection.find_one({"username": current_user})
 
+    if user:
+        # Tìm bản ghi dự đoán theo id
+        prediction = predictions_collection.find_one({"id": prediction_id})
+        if prediction and str(prediction.get("userId")) == str(user.get("idUser")):
+            # Xóa bản ghi nếu người dùng có quyền
+            predictions_collection.delete_one({"id": prediction_id})
+            return jsonify({"message": "Prediction with id {} deleted successfully".format(prediction_id)}), 200
+        else:
+            # Nếu bản ghi không tồn tại hoặc người dùng không có quyền
+            return jsonify({"error": "Prediction not found or access denied"}), 404
+    else:
+        # Nếu thông tin người dùng không tồn tại trong DB
+        return jsonify({"error": "User not found"}), 404
+    
+@app.route('/api/get_prediction/<int:prediction_id>', methods=['GET'])
+@jwt_required()
+def get_prediction(prediction_id):
+    current_user = get_jwt_identity()
+    user = users_collection.find_one({"username": current_user})
+
+    if user:
+        # Truy vấn bản ghi dự đoán theo id
+        prediction = predictions_collection.find_one({"id": prediction_id}, {"_id": 0})
+        if prediction:
+            # Kiểm tra người dùng có quyền xem bản ghi này không
+            if str(prediction.get("userId")) == str(user.get("idUser")):
+                return jsonify({"data":prediction}), 200
+            else:
+                # Truy cập bị từ chối nếu người dùng không có quyền
+                return jsonify({"error": "Access denied"}), 403
+        else:
+            # Bản ghi không tồn tại
+            return jsonify({"error": "Prediction not found"}), 404
+    else:
+        # Người dùng không tìm thấy trong cơ sở dữ liệu
+        return jsonify({"error": "User not found"}), 404
+    
+@app.route('/api/search/predictions', methods=['GET'])
+@jwt_required()
+def get_predictions_by_disease():
+    current_user = get_jwt_identity()
+    user = users_collection.find_one({"username": current_user})
+    
+    if user:
+        # Lấy tham số truy vấn 'disease'
+        disease_name = request.args.get('disease', type=str)
+        # Lấy tham số phân trang 'page' và 'limit', đặt giá trị mặc định nếu không được cung cấp
+        current_page = request.args.get('page', 1, type=int)
+        limit = request.args.get('limit', 10, type=int)
+        # Tính toán chỉ số bắt đầu cho các bản ghi của trang hiện tại
+        skip = (current_page - 1) * limit
+
+        if disease_name:
+            # Tính tổng số bản ghi
+            total = predictions_collection.count_documents({"disease": disease_name})
+            # Tìm và lấy các bản ghi với giới hạn và bỏ qua
+            predictions = predictions_collection.find({"disease": disease_name}, {"_id": 0}).sort("time", -1).skip(skip).limit(limit)
+            predictions_list = list(predictions)
+            
+            if predictions_list:
+                # Tính tổng số trang
+                total_pages = (total + limit - 1) // limit
+                return jsonify({
+                    "total_records": total,
+                    "total_pages": total_pages,
+                    "current_page": current_page,
+                    "limit": limit,
+                    "predictions": predictions_list
+                }), 200
+            else:
+                return jsonify({"message": "No predictions found for the given disease"}), 404
+        else:
+            return jsonify({"error": "Disease parameter is required"}), 400
+    else:
+        return jsonify({"error": "User not found"}), 404
 if __name__ == '__main__':
     app.run(debug=True)
